534e433165775f7ccb81b921eceb9d00852d1b02
ReloadBot <gaaahzl@hotmail.com>
2015-04-13 22:32:24 -0300

    Changes

--- J4 Reloaded/ReloadedJarvan/Properties/AssemblyInfo.cs
+++ J4 Reloaded/ReloadedJarvan/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("StonedJarvan")]
+[assembly: AssemblyTitle("Reloaded Jarvan")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("StonedJarvan")]
+[assembly: AssemblyProduct("Reloaded Jarvan")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
